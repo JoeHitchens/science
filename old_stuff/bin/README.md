ff33017d7fc3fe2047f4cbb8ce707e26b1226071
Joe Hitchens <joe@sleepless.com>
2016-09-13 18:29:09 -0700

    -

--- old_stuff/bin/README.md
+++
@@ -1,4 +0,0 @@
-GTseq-Pipeline
-==============
-
-A series of perl and python scripts for generating genotypes from NGS fastq files from GTseq libraries. 
--- old_stuff/bin/flatten.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-
-mkdir -p flattened
-
-rm -f "./flattened/*.fastq" "./flattened/*.fastq.gz"
-
-for i in `find . | egrep "\.fastq\.gz"` ; do b=`basename "$i"` ; echo "$i" ; cp "$i" "flattened/$b" ; gunzip -f "flattened/$b" ; done
-
