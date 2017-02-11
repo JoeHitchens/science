6441a577b1d5c4061420d3e0829c202492efbd64
Joe Hitchens <joe@sleepless.com>
2017-02-11 14:09:56 -0800

    -

--- scienced.js
+++ scienced.js
@@ -33,32 +33,65 @@ var is_dir = function(path) {
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-var start = function(path) {
-	log("path="+path);
-	fs.unlink(path);
+
+var working = 0;		// number of jobs currently in working state
+
+
+var work = function(name) {
+	return 0;
+}
+
+
+// start processing a job called "name"
+var start = function(name) {
+
+	working += 1;
+
+	log("starting job: "+name);
+
+	fs.renameSync("start/"+name, "working/"+name);		// move to working dir
+
+	// XXX do the work
+	var r = work(name);
+
+	// move to finished or failed dir
+	if(r == 0) {
+		log("finished: "+name);
+		fs.renameSync("working/"+name, "finished/"+name)
+	}
+	else {
+		log("**** FAILED: "+name);
+		fs.renameSync("working/"+name, "failed/"+name)
+	}
+
+	working -= 1;
 }
 
 
 var tick = function() {
+	log("(working on "+working+" jobs)");
 
-	var dir = "./work/start"
-	var files = fs.readdirSync(dir)
-	files.forEach(function(file) {
-		var path = dir + "/" + file;
-		if(is_dir(path) {
-			log("Starting job: "+path);
-			setTimeout(start, 1 * 1000, path);
+	if(working < 1) {		// XXX change to num cpus
+
+		var files = fs.readdirSync("start")
+		for(var i = 0; i < files.length; i++) {
+			var name = files[i];
+			if(is_dir("start/" + name)) {
+				start(name);
+				break;
+			}
 		}
-		else {
-			log("ignoring file: "+path);
-		}
-	});
+
+	}
 
 }
 
 
 
 // start the daemon running
+log("FISHY SCIENCE DAEMON");
+log("Starting "+(new Date()));
+process.chdir("./jobs");
 setInterval(tick, 2 * 1000);
 
 
