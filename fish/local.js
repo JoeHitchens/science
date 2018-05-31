169a85208342fc8db12a69ee807edc04bf155910
aws <aws@sleepless>
2018-05-30 20:32:26 -0700

    -

--- fish/local.js
+++ fish/local.js
@@ -1,6 +1,45 @@
 
 
 
+let sticky_msg = null;
+
+
+function db(sql, args, cb) {
+	$(".busy").show();
+	let data = { user: "scientist", pass: "SybJKzvVydbFThvD", dbname: "science", sql: sql, args: args };
+	obj = { data: JSON.stringify(data) };
+	url = "https://sleepless.com/api/v1/sleepless/db/mysql";
+	$.get(url, obj, (r)=>{
+		$(".busy").hide();
+		cb(r);
+	});
+}
+
+
+function save(fish, cb) {
+	log("saving");
+
+	let form = document.forms.edit;
+
+	let args = [];
+	let f = [];
+	for(var i = 0; i < form.length; i++) {
+		let el = form[i];
+		f.push(el.name+"=?");
+		//args.push(el.name);
+		args.push(el.value);
+	}
+
+	let sql = "update fish set "+f+" where nwfsc=? limit 1";
+	args.push(fish.nwfsc);
+
+	log(sql+"\n"+args);
+	db(sql, args, (r)=>{
+		sticky_msg = null;
+		cb();
+	});
+}
+
 
 function search( frag ) {
 
@@ -15,32 +54,49 @@ function search( frag ) {
 		return;
 	}
 
-	let data = { user: "scientist", pass: "SybJKzvVydbFThvD", dbname: "science" };
-	data.sql = "select * from fish where nwfsc like ? order by nwfsc";
-	data.args = ["%"+frag+"%"];
-	obj = { data: JSON.stringify(data) };
-	url = "https://sleepless.com/api/v1/sleepless/db/mysql";
-	$.get( url, obj, (r)=>{
+	let sql = "select * from fish where nwfsc like ? order by nwfsc";
+	let args = ["%"+frag+"%"];
+	db(sql, args, (r)=>{
 		if(r.error) { alert(r.error); return; }
 		replicate("tpl_search_result", r.records, (e, fish, i)=>{
 			e.onclick = function() {
-
+				
 				replicate("tpl_details", [fish]);
+				document.forms.edit.onchange = ()=>{
+					log("modified");
+					sticky_msg = "Unsaved changes.";
+					$(".save_btn").addClass("accent");
+				};
+
 				$("div.page[name=edit]").dialog({
 					title: "A Fish Called "+fish.nwfsc,
 					modal: true,
 					width: "auto",
 					buttons: [
 						{
+							class: "save_btn",
 							text: "Save",
 							click: function() {
+								let dlg = this;
 								save(fish, function() {
-									$(this).dialog("close");
+									$(dlg).dialog("close");
 									reload();
 								});
 							},
 						},
 					],
+					beforeClose: (evt, ui)=>{
+						log(sticky_msg);
+						if(sticky_msg) {
+							if(!confirm(sticky_msg)) {
+								return false;
+							}
+						}
+						return true;
+					},
+					open: (evt, ui)=>{
+						$(".save_btn").removeClass("accent");
+					},
 				});
 			}
 		});
@@ -66,7 +122,12 @@ $(document).ready(()=>{
 		return false;
 	};
 
+	f_edit.onsubmit = function() {
+		return false;
+	};
+
 	let qd = getQueryData();
 	search(qd.search_frag || "");
 
+	window.onbeforeunload = ()=>{ return sticky_msg };
 });
